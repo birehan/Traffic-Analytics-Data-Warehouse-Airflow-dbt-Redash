cb2740496a324b5452c2c591d8dfa62d9f1e8f8a
birehan <birehananteneh4@gmail.com>
2023-12-20 01:56:44 +0300

    feat: load data to database using dag

--- dags/create_tables_dag.py
+++ dags/create_tables_dag.py
@@ -1,8 +1,10 @@
-from datetime import datetime, timedelta
+from datetime import  timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from sqlalchemy import create_engine
+import pandas as pd
 import os
+import logging
 
 default_args = {
     'owner': 'BirehanAnteneh',
@@ -49,17 +51,41 @@ create_vehicle_data_table_task = PythonOperator(
     dag=dag,
 )
 
-# Task to create the detailed_vehicle_info table
-create_detailed_vehicle_info_table_task = PythonOperator(
-    task_id='create_detailed_vehicle_info_table_task',
-    python_callable=execute_sql_script,
-    op_kwargs={'script_path': './dags/sql/create_vehicle_detail_table.sql'},
+def insert_data_into_table():
+    # Log the current working directory
+    current_working_directory = os.getcwd()
+    logging.info(f"Current Working Directory: {current_working_directory}")
+
+    db_connection_url = 'postgresql+psycopg2://airflow:airflow@postgres/airflow'
+    engine = create_engine(db_connection_url)
+
+    # Specify the relative path to the CSV file
+    csv_file_relative_path = './dags/data/dataset.csv'
+
+    # Get the absolute path
+    csv_file_absolute_path = os.path.abspath(csv_file_relative_path)
+    logging.info(f"Absolute Path to CSV File: {csv_file_absolute_path}")
+
+    # Read data from CSV file
+    df = pd.read_csv(csv_file_relative_path, sep="[,;:]", index_col=False)
+
+    # Insert data into the table
+    table_name = 'vehicle_data'
+    df.to_sql(table_name, engine, if_exists='replace', index=False)
+  
+
+
+insert_data_task = PythonOperator(
+    task_id='insert_data_task',
+    python_callable=insert_data_into_table,
     dag=dag,
 )
 
+
+
 # Set task dependencies
-create_airflow_db_task >> create_vehicle_data_table_task
-create_vehicle_data_table_task >> create_detailed_vehicle_info_table_task
+create_airflow_db_task >> create_vehicle_data_table_task >> insert_data_task
 
 if __name__ == "__main__":
     dag.cli()
+# /home/babi/Desktop/10academy/Traffic-Analytics-Data-Warehouse-Airflow-dbt-Redash/data/dataset.csv
